95ae8bb1def0fddc459dc86f20ec74f3aca9032c
salma rami <ramisalma185@gmail.com>
2025-10-08 14:29:51 +0100

    commit 12

--- tst.jsx
+++ tst.jsx
@@ -1,2 +1,23 @@
-const props = undefined;
-return <Component {...props} />;
+const initialState = {
+  users: [],
+  loading: false,
+  error: null
+};
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_USERS_START:
+      return { ...state, loading: true, error: null };
+
+    case FETCH_USERS_SUCCESS:
+      return { ...state, loading: false, users: action.payload };
+
+    case FETCH_USERS_ERROR:
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
